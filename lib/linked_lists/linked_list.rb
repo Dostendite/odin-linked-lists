ddd37dafc618d5e0bba1a0786e95d2a1a0b90247
Dostendite <dostendite@outlook.es>
2024-07-02 11:04:11 +0200

    Add methods from assignment

--- lib/linked_lists/linked_list.rb
+++ lib/linked_lists/linked_list.rb
@@ -2,4 +2,49 @@ class LinkedList
     def initialize
         @head = nil
     end
+
+    def add_first(value)
+        @head = node
+    end
+
+    def append(value)
+        # traverse and append
+    end
+
+    def prepend(value)
+        # prepend and reassign head
+    end
+
+    def size
+        # create a counter and traverse
+    end
+
+    def head
+        puts "#{@ħead}"
+    end
+
+    def tail
+        # traverse and retun the tail
+    end
+
+    def at(index)
+        # return node at index
+    end
+
+    def pop
+        # remove the last element from the list
+    end
+
+    def contains?(value)
+        # return true if value is in linked list
+    end
+
+    def find(value)
+        # return index if value is in linked list
+    end
+
+    def to_s
+        # represent your LL objects as strings
+        # (value) -> (value) -> (value) -> nil
+    end
 end
